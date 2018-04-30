1eacf3e5b02adbde21df6b02fb1e89f0ac4009e1
jimmycgz <38402619+jimmycgz@users.noreply.github.com>
2018-04-30 18:51:56 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 # Test by Jmy, changed to GitHub Webhook, use Jenkins Pipeline, add "/" after hook url, test GitHub Org Proj in Jks, corrected branch Canary to lower case
 # Copyright 2015 Google Inc. All rights reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License,  Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
